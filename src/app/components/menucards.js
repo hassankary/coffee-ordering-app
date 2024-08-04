d88c57c7896e33eedc165fb9f0a9a08a79f8df80
Hassan Kaeru <hassanaskary29@gmail.com>
2024-08-04 11:43:45 +0700

    update card style

--- src/app/components/menucards.js
+++ src/app/components/menucards.js
@@ -6,23 +6,21 @@ import { FaEye } from "react-icons/fa6";
 import { BsFillBookmarkHeartFill } from "react-icons/bs";
 import { IoHeartCircleSharp } from "react-icons/io5";
 
-
-
 export const MenuCards = ({
   onClickModal,
   onClickMinus,
   onClickPlus,
   data,
-  props
+  props,
 }) => {
   return (
     <div {...props} className="flex flex-col bg-[#FFFFFF] rounded-xl shadow-lg">
       <div className="flex flex-col group">
-      {data?.favorite && (
-        <div className="relative -mb-10 self-end w-fit z-10 p-1">
-          <IoHeartCircleSharp className="float-right w-8 h-8 fill-red-600" />
-        </div>
-      )}
+        {data?.favorite && (
+          <div className="relative -mb-10 self-end w-fit z-10 p-1">
+            <IoHeartCircleSharp className="float-right w-8 h-8 fill-red-600" />
+          </div>
+        )}
         <Image
           id={data?.id}
           onClick={onClickModal}
@@ -34,34 +32,46 @@ export const MenuCards = ({
           quality={100}
           unoptimized
         />
-        <div className="flex opacity-0 group-hover:opacity-100 -mt-6 h-6 justify-center items-center text-white bg-black bg-opacity-60 rounded-t-xl transition-all duration-300">
+        <div onClick={onClickModal} className="flex opacity-0 group-hover:opacity-100 -mt-6 h-6 justify-center items-center text-white bg-black bg-opacity-60 rounded-t-xl transition-all duration-300">
           <FaEye />
         </div>
       </div>
-      <div className="p-4 font-medium overflow-hidden">
+      <div className="p-4 font-semibold overflow-hidden">
         <h1 className="text-[#333736] text-nowrap truncate">{data?.name}</h1>
-        <div className="flex flex-row flex-wrap justify-between items-center">
-          <h1 className="flex text-sm items-center text-green-600">
-            Rp {data?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
+        <div className="flex flex-row flex-wrap mt-2 justify-between items-center">
+          <h1 className="flex text-base items-center font-bold text-green-600">
+            {data?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
           </h1>
-          <div className=" w-[70px] flex flex-row justify-between">
-            <button
-              id={data?.id}
-              onClick={(e) => onClickMinus(e, data?.id)}
-              className="flex w-[25px] h-[25px] bg-[#EAB968] hover:bg-[#f5c16f] active:bg-[#cda15a] justify-center items-center rounded-full transition-all"
-            >
-              <HiOutlineMinus />
-            </button>
-            <p className="flex text-sm text-black justify-center items-center">
-              {data?.amount}
-            </p>
-            <button
-              id={data?.id}
-              onClick={(e) => onClickPlus(e, data?.id)}
-              className="flex w-[25px] h-[25px] bg-[#EAB968] hover:bg-[#f5c16f] active:bg-[#cda15a] justify-center items-center rounded-full transition-all"
-            >
-              <HiOutlinePlus />
-            </button>
+          <div className={`w-[76px] flex flex-row transition-all`}>
+            {data.amount === 0 ? (
+              <button
+                id={data?.id}
+                onClick={(e) => onClickPlus(e, data?.id)}
+                className="flex w-full h-[32.6px] border bg-green-600 hover:bg-green-500 active:bg-green-500 justify-center items-center rounded-full transition-all"
+              >
+                Order
+              </button>
+            ) : (
+              <div className="flex w-full p-[3px] border justify-between rounded-full transition-all">
+                <button
+                  id={data?.id}
+                  onClick={(e) => onClickMinus(e, data?.id)}
+                  className="flex w-[25px] h-[25px] bg-green-600 hover:bg-green-500 active:bg-green-500 justify-center items-center rounded-full transition-all"
+                >
+                  <HiOutlineMinus />
+                </button>
+                <p className="flex text-sm text-black justify-center items-center">
+                  {data?.amount}
+                </p>
+                <button
+                  id={data?.id}
+                  onClick={(e) => onClickPlus(e, data?.id)}
+                  className="flex w-[25px] h-[25px] bg-green-600 hover:bg-green-500 active:bg-green-500 justify-center items-center rounded-full transition-all"
+                >
+                  <HiOutlinePlus />
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import { BiSolidShoppingBag } from "react-icons/bi";
+import { SiAdblock } from "react-icons/si";
 
 export default function Home() {
   return (
     <>
       <title>Coffee Ordering Mobile Web by Hassan Kaeru</title>
       <div className=" w-full flex justify-center">
-        <div className="min-h-screen w-[414px] bg-green-500 content-center">
+        <div className="flex flex-col justify-center min-h-screen w-[414px] bg-green-500 content-center">
           <div className="flex justify-center ">
             <Image
               src={"/./logo.svg"}
@@ -20,7 +21,7 @@ export default function Home() {
               priority
             />
           </div>
-          <div className="flex justify-center mt-[90px]">
+          <div className="flex flex-col items-center justify-center mt-[90px]">
             <Link
               href={"./pages/order"}
               className="p-5 bg-black text-black font-bold hover:bg-[#EAB968] hover:scale-110 active:scale-110 rounded-full transition-all duration-300"
@@ -29,6 +30,12 @@ export default function Home() {
                 <BiSolidShoppingBag className="h-[40px] w-[40px]" />
               </h1>
             </Link>
+          </div>
+          <div className="fixed flex w-full max-w-[414px] justify-end bottom-0">
+            <button className="flex justify-center items-center px-3 py-1.5 font-bold space-x-2 bg-[#EAB968] hover:bg-[#e1ac57] active:scale-110 rounded-tl-xl transition-all">
+              <SiAdblock/>
+              <h1>Admin</h1>
+            </button>
           </div>
         </div>
       </div>
